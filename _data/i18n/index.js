e86d64f6a90eae35543dabb6686a900bf3c91f27
May Márquez <maymiami20@gmail.com>
2022-02-17 19:01:41 -0500

    add internationalization for spanish

---
+++ _data/i18n/index.js
@@ -0,0 +1,103 @@
+module.exports = {
+    carpentry: {
+        'en-US': 'Carpentry Supplies',
+        'es': 'Suministros de Carpintería'
+    },
+    carpentrySubtitle: {
+        'en-US': 'Plywood | Hardware | Laminates',
+        'es': 'Madera | Hardware | Laminados'
+    },
+    materials: {
+        'en-US': 'MATERIALS',
+        'es': 'MATERIALES'
+    },
+    materialsSubtitle: {
+        'en-US': 'We supply all the tools and materials necessary to get your job done right.',
+        'es': 'Suministramos todas las herramientas y materiales necesarios para hacer bien su trabajo.'
+    },
+    navMaterials: {
+        'en-US': 'Materials',
+        'es': 'Materiales'
+    },
+    navHome: {
+        'en-US': 'Home',
+        'es': 'Inicio'
+    },
+    navLocation: {
+        'en-US': 'Location',
+        'es': 'Ubicación'
+    },
+    navContact: {
+        'en-US': 'Contact Us',
+        'es': 'Contáctenos'
+    },
+    findUs: {
+        'en-US': 'FIND US',
+        'es': 'ENCUENTRANOS'
+    },
+    dates: {
+        'en-US': 'Monday<br>Tuesday<br>Wednesday<br>Thursday<br>Friday<br>Saturday<br>Sunday<br>',
+        'es': 'Lunes<br>Martes<br>Miércoles<br>Jueves<br>Viernes<br>Sábado<br>Domingo<br>'
+    },
+    contactUs: {
+        'en-US': 'GET IN TOUCH',
+        'es': 'CONTÁCTENOS'
+    },
+    contactUsSubtitle: {
+        'en-US': 'Call us at <a class="burgundy" href="tel:7863611073"><b>786-361-1073</b></a> or leave us a message using the form below',
+        'es': 'Llámanos al <a class="burgundy" href="tel:7863611073"><b>786-361-1073</b></a> o déjanos un mensaje usando el siguiente formulario'
+    },
+    email: {
+        'en-US': 'Your email address',
+        'es': 'Tu correo electrónico'
+    },
+    emailSubtitle: {
+        'en-US': 'We\'ll never share your email with anyone else.',
+        'es': 'Nunca compartiremos su correo electrónico con nadien'
+    },
+    requiredFields: {
+        'en-US': 'All fields, unless otherwise noted, are required.',
+        'es': 'Todos los campos, a menos que se indique lo contrario, son obligatorios.'
+    },
+    name: {
+        'en-US': 'Full name',
+        'es': 'Nombre completo'
+    },
+    phone: {
+        'en-US': 'Phone number',
+        'es': 'Número de teléfono'
+    },
+    optional: {
+        'en-US': '(optional)',
+        'es': '(opcional)'
+    },
+    address: {
+        'en-US': 'Address',
+        'es': 'Dirección'
+    },
+    subject: {
+        'en-US': 'Subject',
+        'es': 'Sujeto'
+    },
+    message: {
+        'en-US': 'Message (markdown allowed)',
+        'es': 'Mensaje (markdown permitidas)'
+    },
+    messageSubtitle: {
+        'en-US': 'Type your message here...',
+        'es': 'Escribe tu mensaje aquí...'
+    },
+    submit: {
+        'en-US': 'Submit',
+        'es': 'Enviar'
+    },
+    pageSlug: {
+        'en-US': '/es',
+        'es': '/en-US'
+    },
+    language: {
+        'en-US': 'Español',
+        'es': 'English'
+    }
+
+}
--- js/index.js
+++ js/index.js
@@ -1,24 +1,45 @@
 // nullify the global reference while instrumenting the form
 StaticEmail = (function (StaticEmail) {
-    contact.addEventListener('submit', function (event) {
-      event.preventDefault();
-      var from = contact.from.value.trim();
-      var md = contact.md.value.trim();
-      var name = contact.name.value.trim();
-      var subject = contact.subject.value.trim();
-      // optional params
-      var phone = contact.phone.value.trim();
-      var address = contact.address.value.trim();
-      phone = (phone) ? phone : 'Not specified';
-      address = (address) ? address : 'Not specified';
-
-      if (from && md) {
-        contact.submit.disabled = true;
-        StaticEmail({
-          path: '/api/paperboy',
-          subject: (subject) ? subject : 'New message from ' + from + ' through your website\'s contact form.',
-          from: from,
-          md: 
+  // we don't know when window.recaptcha will be called,
+  // so we use a promise to simplify our workflow
+  var captcha = new Promise(function (resolve) {
+    window.recaptcha = function () {
+      // resolve passing the widget id along
+      resolve(
+        grecaptcha.render('recaptcha', {
+          // the SITE KEY reCAPTCHA provided
+          sitekey : '6LdV0oQeAAAAAKk6R2mrBmnEN908uZ6XhtPt9wTQ'
+        })
+      );
+    };
+  });
+  contact.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var from = contact.from.value.trim();
+    var md = contact.md.value.trim();
+    var name = contact.name.value.trim();
+    var subject = contact.subject.value.trim();
+    // optional params
+    var phone = contact.phone.value.trim();
+    var address = contact.address.value.trim();
+    phone = (phone) ? phone : 'Not specified';
+    address = (address) ? address : 'Not specified';
+    if (from && md && name) {
+      // asynchronous operations, better block the submit
+      contact.submit.disabled = true;
+      // whenever the external script lands ...
+      captcha.then(function (widget) {
+        // verify it's been clicked
+        var recaptcha = grecaptcha.getResponse(widget);
+        // if that's the case ...
+        if (recaptcha) {
+          // ensure it doesn't have a red outline anymore
+          document.querySelector('#recaptcha').className = '';
+          StaticEmail({
+            path: '/api/paperboy',
+            subject: (subject) ? subject : 'New message from ' + from + ' through your website\'s contact form.',
+            from: from,
+            md:     
 `A visitor to your site, **dariosusa.com**, has left you a message through your contact form.
 
 **Please send replies to the sender's email address in a new email thread. Do not reply to this email. This inbox is not monitored.**
@@ -38,21 +59,31 @@ StaticEmail = (function (StaticEmail) {
 ---
 
 # Message:
-${md}`
-        })
-        .then(
-          thanks,
-          function (error) {
-            alert(error);
-            contact.submit.disabled = false;
-          }
-        );
-      }
-    });
-    function thanks() {
-      var p = document.createElement('p');
-      p.textContent = 'Thank you for your message! We will get back to you soon.';
-      p.className = 'text-center burgundy';
-      contact.parentNode.replaceChild(p, contact);
+${md}`,
+            // and pass the recaptcha token to StaticEmail
+            recaptcha: recaptcha
+          })
+          .then(
+            thanks,
+            function (error) {
+              alert(error);
+              contact.submit.disabled = false;
+            }
+          );
+        }
+        // if reCAPTCHA was not clicked, outline it red
+        // to indicate it's mandatory (or use any other indication you like)
+        else {
+          document.querySelector('#recaptcha').className = 'invalid';
+          contact.submit.disabled = false;
+        }
+      });
     }
-  }(StaticEmail));
+  });
+  function thanks() {
+    var p = document.createElement('p');
+    p.textContent = 'Thank you for your message! We will get back to you soon.';
+    p.className = 'text-center burgundy';
+    contact.parentNode.replaceChild(p, contact);
+  }
+}(StaticEmail));
